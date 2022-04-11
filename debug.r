1f7fdef437bbdb286fcd8b86ca6ae119d411391f
Gwladys Bareth <102359774+Alcetylcysteine@users.noreply.github.com>
2022-04-11 12:00:31 +0200

    Update debug.r
    Changement des return, 
    Création des nouvelles colonnes dans la boucle for et plus dans la fonction. 
    Toujours même erreur au niveau de la ligne 25 : 
    [1] 24
    [1] "Version UICC non connue" ""                       
    # A tibble: 1 x 1
      STADE                  
      <chr>                  
    1 Version UICC non connue
    Error in if (Registre_poumon[i, "PT"] == "" & Registre_poumon[i, "PN"] ==  : 
      missing value where TRUE/FALSE needed
    In addition: There were 24 warnings (use warnings() to see them)
    
    
    > warnings()
    Messages d'avis :
    1: In if (is.na(Registre_poumon[i, "UICC"])) { ... :
      the condition has length > 1 and only the first element will be used
    2: In if (is.na(Registre_poumon[i, "UICC"])) { ... :
      the condition has length > 1 and only the first element will be used
    3: In if (is.na(Registre_poumon[i, "UICC"])) { ... :
      the condition has length > 1 and only the first element will be used
    4: In if (is.na(Registre_poumon[i, "UICC"])) { ... :
      the condition has length > 1 and only the first element will be used
    5: In if (is.na(Registre_poumon[i, "UICC"])) { ... :
      the condition has length > 1 and only the first element will be used

--- debug.r
+++ debug.r
@@ -1,1 +1,97 @@
-Mets moi ton code ici
+library(readxl)
+Registre_poumon <- read_excel("R:/Masters/Stage_G_BARETH/etude_POUMON/poumon_selec_copie.xls")
+
+Stade_TNM <- function(i, Tx, Nx, Mx) {
+  
+  # Si l'UICC est inconnu alors : 
+  if(is.na(Registre_poumon[i,"UICC"])){
+    return(c("Version UICC non connue",""))}
+  
+  # Si l'UICC = 8 alors on rentre dans un  nouveau if/else : 
+  else if (Registre_poumon[i,"UICC"] == 8) {
+    
+    Tx <- paste("T", Registre_poumon[i,"PT"], sep = "")
+    Nx <- paste("N", Registre_poumon[i,"PN"], sep = "")
+    
+    # Première étape : importer le tableau de classification TNM correspondant à la bonne version UICC. 
+    library(readxl)
+    Tableau_TNM <- read_excel("R:/Masters/Stage_G_BARETH/etude_POUMON/Classification TNM du cancer du poumon par stades.xlsx")
+    # Si le tableau_TNM n'est pas mis sous forme de data-frame, voici le message d'erreur : Warning message:
+    # Setting row names on a tibble is deprecated. 
+    Tableau_TNM <- as.data.frame(Tableau_TNM)
+    row.names(Tableau_TNM) <-  c("T1a", "T1b", "T1c", "T2a", "T2b", "T3", "T4")
+    
+    if (Registre_poumon[i,"PT"] == "" & Registre_poumon[i,"PN"]== "" & Registre_poumon[i,"PM"]== "" ){
+      return(c("on utilise le cTNM",""))
+      } 
+    else if (Registre_poumon[i,"PT"] == "0" & Registre_poumon[i,"PN"]== "0" & Registre_poumon[i,"PM"]== "0" ){
+      return(c("on utilise le cTNM",""))
+      }
+    else if (Registre_poumon[i,"PT"] == "y0" | Registre_poumon[i,"PT"] == "y1" | Registre_poumon[i,"PT"] == "y1a" | Registre_poumon[i,"PT"] == "y1b" | Registre_poumon[i,"PT"] == "y1c" | Registre_poumon[i,"PT"] == "y2a" | Registre_poumon[i,"PT"] == "y2b" | Registre_poumon[i,"PT"] == "y3" | Registre_poumon[i,"PT"] == "y4" | Registre_poumon[i,"PN"] == "y0" | Registre_poumon[i,"PN"] == "y1" | Registre_poumon[i,"PN"] == "y2" | Registre_poumon[i,"PN"] == "y3") {
+      return(c("on utilise le cTNM", "neo-adjuvant"))
+    }
+    ## on utilise le pTNM
+    else { 
+    
+    #### Trouver le M #### 
+    # Si le M est différent de NULL ou différent de 8 dans ce cas : 
+    if (Registre_poumon[i,"PM"] != "" & Registre_poumon[i,"PM"] != "8") {
+      if (Registre_poumon[i,"PM"] == "1c") {
+        return(c("Stade IV-B", ""))
+        }
+      else if (Registre_poumon[i,"PM"] == "1") {
+        return(c("Stade IV-B", "imprécis")) 
+        }
+      else {
+        return(c("Stade IV-A", ""))
+        }
+    }
+    else if (Registre_poumon[i,"PM"] != "y1a") { return("Voir le cTNM","") }
+    else {
+      if(Registre_poumon[i,"PT"] == "0" | Registre_poumon[i,"PT"] == "1" | Registre_poumon[i,"PT"] == "11a" | Registre_poumon[i,"PT"] == "1mi" | Registre_poumon[i,"PT"] == "1A"){ 
+        Tx <- "T1a"}
+      else if (Registre_poumon[i,"PT"] == "2") { 
+        Tx <- "2a"}
+      return(Tableau_TNM[Tx,Nx],"")
+    }
+    }
+   }
+  
+  #Si l'UICC est une version antérieure alors : 
+  else {return(c("Version UICC antérieure", ""))}
+    
+  }
+
+
+for (i in 1:nrow(Registre_poumon)) {
+  
+  Tx <- Registre_poumon[i,"PT"]
+  Nx <- Registre_poumon[i,"PN"]
+  Mx <- Registre_poumon[i,"PM"]
+    
+  Stade_TNM(i, Tx, Nx, Mx)
+ print(i)
+ print(Stade_TNM())
+ Registre_poumon[i,"STADE"] <- Stade_TNM(i,Tx,Nx,Mx)[1]
+ print(Registre_poumon[i,"STADE"])
+ Registre_poumon[i,"STADE_PRECISION"] <- Stade_TNM(i,Tx,Nx,Mx)[2]
+ 
+    }
+
+
+
+####################### VERIFICATIONS #########################
+
+###### Différentes valeurs de PM : #######
+#   table(Registre_poumon[,"PM"], useNA = "always")
+#     1    1a    1b    1c     8   y1a  <NA> 
+#   435   472   839   356     2     1 13483
+
+
+
+###### Différentes valeurs de PT : ######
+# 0     1   11a    1a    1A    1b    1c   1mi     2    24    2a    2b     3     4     7     8     x    y0    y1   y1a   y1b   y1c 
+# 1   184     1   488     1   486   136     6   234     1   676   199   460   189     1   258     1    10     9    13     9     5 
+# 
+#   y2   y2a   y2b    y3    y4  yt2a  <NA> 
+#   12    24     8    28    15     1 12132
